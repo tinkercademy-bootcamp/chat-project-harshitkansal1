65d4d4953b1e6fd4ea68a636c4926bd195441016
harshit kansal <harshitkansal25@gmail.com>
2025-05-27 16:22:53 +0000

    separated common code in a header file

--- exercise-4/src/tcp_echo_client.cpp
+++ exercise-4/src/tcp_echo_client.cpp
@@ -1,24 +1,7 @@
 #include <arpa/inet.h>
 #include <cstdlib>
-#include <iostream>
-#include <netinet/in.h>
 #include <string>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-void check_error(bool test, std::string error_message) {
-  if (test) {
-    std::cerr << error_message << "\n";
-    exit(EXIT_FAILURE);
-  }
-}
-
-int create_socket() {
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  check_error(sock < 0, "Socket creation error\n");
-  return sock;
-}
+#include "common.h"
 
 sockaddr_in create_address(const std::string &server_ip, int port) {
   sockaddr_in address;
--- exercise-4/src/tcp_echo_server.cpp
+++ exercise-4/src/tcp_echo_server.cpp
@@ -1,22 +1,4 @@
-#include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-template <typename T, typename S> void check_error(T test, S error_message) {
-  if (test) {
-    std::cerr << error_message << "\n";
-    exit(EXIT_FAILURE);
-  }
-}
-
-int create_socket() {
-  int my_sock;
-  my_sock = socket(AF_INET, SOCK_STREAM, 0);
-  check_error(my_sock < 0, "Socket creation error\n");
-  return my_sock;
-}
+#include"common.h"
 
 void set_socket_options(int sock, int opt) {
   auto err_code = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
