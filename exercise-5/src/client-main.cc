bf4bedd337ed08eff61ac618417bf48541753822
harshit kansal <harshitkansal25@gmail.com>
2025-05-28 06:17:27 +0000

    change client code to use a class

--- exercise-5/src/client-main.cc
+++ exercise-5/src/client-main.cc
@@ -10,33 +10,43 @@
 #include "net/chat-sockets.h"
 #include "utils.h"
 
-sockaddr_in create_server_address(const std::string &server_ip, int port) {
+class Client {
+  private:
+  int client_socket;
+  sockaddr_in  server_address;
+
+  public:
+  void create_socket(){
+    client_socket = tt::chat::net::create_socket();
+  }
+
+void create_server_address(const std::string &server_ip, int port) {
   using namespace tt::chat;
   sockaddr_in address = net::create_address(port);
   // Convert the server IP address to a binary format
   auto err_code = inet_pton(AF_INET, server_ip.c_str(), &address.sin_addr);
   check_error(err_code <= 0, "Invalid address/ Address not supported\n");
-  return address;
+  server_address = address;
 }
 
-void connect_to_server(int sock, sockaddr_in &server_address) {
+void connect_to_server() {
   using namespace tt::chat;
   auto err_code =
-      connect(sock, (sockaddr *)&server_address, sizeof(server_address));
+      connect(client_socket, (sockaddr *)&server_address, sizeof(server_address));
   check_error(err_code < 0, "Connection Failed.\n");
 }
 
-void send_and_receive_message(int sock, const std::string &message) {
+void send_and_receive_message(const std::string &message) {
   using namespace tt::chat;
   const int kBufferSize = 1024;
   char recv_buffer[kBufferSize] = {0};
 
   // Send the message to the server
-  send(sock, message.c_str(), message.size(), 0);
+  send(client_socket, message.c_str(), message.size(), 0);
   std::cout << "Sent: " << message << "\n";
 
   // Receive response from the server
-  ssize_t read_size = read(sock, recv_buffer, kBufferSize);
+  ssize_t read_size = read(client_socket, recv_buffer, kBufferSize);
   check_error(read_size < 0, "Read error.\n");
   if (read_size > 0) {
     std::cout << "Received: " << recv_buffer << "\n";
@@ -44,6 +54,14 @@ void send_and_receive_message(int sock, const std::string &message) {
     std::cout << "Server closed connection.\n";
   }
 }
+ ~Client() {
+    if (client_socket >= 0) {
+      close(client_socket);
+      std::cout << "Server socket closed.\n";
+    }
+  }
+
+};
 
 std::string read_args(int argc, char *argv[]) {
   using namespace tt::chat;
@@ -64,12 +82,12 @@ int main(int argc, char *argv[]) {
 
   std::string message = read_args(argc, argv);
 
-  int my_socket = tt::chat::net::create_socket();
-  sockaddr_in server_address = create_server_address(kServerAddress, kPort);
+  Client myclient;
+  myclient.create_socket();
+  myclient.create_server_address(kServerAddress, kPort);
 
-  connect_to_server(my_socket, server_address);
-  send_and_receive_message(my_socket, message);
-  close(my_socket);
+  myclient.connect_to_server();
+  myclient.send_and_receive_message(message);
 
   return 0;
 }
